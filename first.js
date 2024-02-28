1048b464226a90dccd2ecc57cb173c7104991f15
Aakashkataria05 <aakashkataria064@gmail.com>
2024-02-29 03:34:18 +0530

    ini

---
+++ first.js
@@ -0,0 +1,15 @@
+console.log("systumm !!");
+console.log("aangli ghmaa denge chotee bhai !!");
+let fullName = "aakash";
+
+
+const product = {
+    title: "Ball pen",
+    rating: 4,
+    offer: 5,
+    price: 270,
+};
+
+console.log(product);
+
+console.log(product);
