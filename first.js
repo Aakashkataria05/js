98bfa79e5a43b0f2c4460e3e0423eeaa7dee7e91
Aakashkataria05 <aakashkataria064@gmail.com>
2024-03-03 22:37:13 +0530

    grade

--- first.js
+++ first.js
@@ -1,27 +1,47 @@
 console.log("systumm !!");
 console.log("aangli ghmaa denge chotee bhai !!");
-let fullName = "aakash";
+// let fullName = "aakash";
 
 
-const product = {
-    title: "Ball pen",
-    rating: 4,
-    offer: 5,
-    price: 270,
-};
+// const product = {
+//     title: "Ball pen",
+//     rating: 4,
+//     offer: 5,
+//     price: 270,
+// };
 
-let a = 4;
-let b = 5;
+// let a = 4;
+// let b = 5;
 
-console.log("a == b && a<b", a==b && a<b);
-console.log("a==b || a<b", a==b || a<b);
-console.log("!(a == b)", !(a == b));
+// console.log("a == b && a<b", a==b && a<b);
+// console.log("a==b || a<b", a==b || a<b);
+// console.log("!(a == b)", !(a == b));
 
 // let name = prompt("hello");
 // console.log(name);
 
 // alert("aakash");
 
+let marks = prompt("enter your marks");
+let grade;
+
+if(marks<=100 && marks>=80){
+    grade = 'A';
+}
+if(marks<=79 && marks>=60){
+    grade = 'B';
+}
+if(marks<=59 && marks>=40){
+    grade = 'C';
+}
+if(marks<=39 && marks>=30){
+    grade = 'D';
+}
+else{
+    grade = 'F';
+}
+
+console.log("your grade is :", grade);
 
 
 
