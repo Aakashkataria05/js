5cb38448ee0b1260a61df9f4d24cce55b8972202
Aakashkataria05 <aakashkataria064@gmail.com>
2024-03-07 03:59:07 +0530

    loop

--- first.js
+++ first.js
@@ -22,30 +22,37 @@ console.log("aangli ghmaa denge chotee bhai !!");
 
 // alert("aakash");
 
-let marks = prompt("enter your marks");
-let grade;
+// let marks = prompt("enter your marks");
+// let grade;
 
-if(marks<=100 && marks>=80){
-    grade = 'A';
-}
-if(marks<=79 && marks>=60){
-    grade = 'B';
-}
-if(marks<=59 && marks>=40){
-    grade = 'C';
-}
-if(marks<=39 && marks>=30){
-    grade = 'D';
-}
-if (condition) {
-    
-} else {
-    
-}
+// if(marks<=100 && marks>=80){
+//     grade = 'A';
+// }
+// if(marks<=79 && marks>=60){
+//     grade = 'B';
+// }
+// if(marks<=59 && marks>=40){
+//     grade = 'C';
+// }
+// if(marks<=39 && marks>=30){
+//     grade = 'D';
+// }
+// else if (marks<=29 && marks>=0) {
+//     grade = 'F'
+// }
 
-console.log("your grade is :", grade);
+// console.log("your grade is :", grade);
 
-number = prompt("enter number");
-console.log("number is:", number);
+// number = prompt("enter number");
+// // console.log("number is:", number);
 
+// let i = 1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i <= number);
 
+let str = 'aakash';
+for (let i of str){
+    console.log(i); 
+}
